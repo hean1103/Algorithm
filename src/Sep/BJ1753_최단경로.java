6adbaef98f7e8ccd667f56fc45d3360e8145a7e5
hean <hean961103@gmail.com>
2021-09-20 16:15:49 +0900

    BJ 1753 최단거리 주석 추가

--- src/Sep/BJ1753_최단경로.java
+++ src/Sep/BJ1753_최단경로.java
@@ -1,5 +1,23 @@
 package Sep;
-
+/*******************************************************************************
+ * BaekJoon 1753 번 
+ * 
+ * 예시 input 
+ * 5 6
+ * 1
+ * 5 1 1
+ * 1 2 2
+ * 1 3 3
+ * 2 3 4
+ * 2 4 5
+ * 3 4 6
+ * 결과 값 
+ * 0
+ * 2
+ * 3
+ * 7
+ * INF
+ */
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -22,7 +40,7 @@ public class BJ1753_최단경로 {
 		}
 		
 		@Override
-		public int compareTo(Node o) {
+		public int compareTo(Node o) { //가중치 내림차순 
 			
 			return this.w - o.w;
 		}
@@ -34,12 +52,12 @@ public class BJ1753_최단경로 {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		int V, E, S;
 		StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
-		V = Integer.parseInt(st.nextToken());
+		V = Integer.parseInt(st.nextToken()); 
 		E = Integer.parseInt(st.nextToken());
 		
 		ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
 		for (int i = 0; i <= V; i++) {
-			graph.add(new ArrayList<Node>());
+			graph.add(new ArrayList<Node>()); // 초기화 
 		}
 		st = new StringTokenizer(bf.readLine(), " ");
 		S = Integer.parseInt(st.nextToken());
@@ -50,13 +68,13 @@ public class BJ1753_최단경로 {
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
 			
-			graph.get(u).add(new Node(v, w));
+			graph.get(u).add(new Node(v, w)); // 노드 연결 
 		}
 		
-		int[] d = Dijkstra(S, V, graph);
+		int[] d = Dijkstra(S, V, graph); // 다익스트라 실행  
 		
 		for (int i = 1; i <= V; i++) {
-			if(d[i] == Integer.MAX_VALUE) bw.write("INF\n");
+			if(d[i] == Integer.MAX_VALUE) bw.write("INF\n"); // 최대값이 저장되어 있으면 연결 안됨을 표시 
 			else {
 				bw.write(String.valueOf(d[i]) + "\n");
 			}
@@ -65,26 +83,26 @@ public class BJ1753_최단경로 {
 	}
 	
 	static int[] Dijkstra(int start, int V, ArrayList<ArrayList<Node>> graph) {
-		PriorityQueue<Node> pq = new PriorityQueue<>();
-		int[] dist = new int[V+1];
-		boolean[] v = new boolean[V+1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
+		PriorityQueue<Node> pq = new PriorityQueue<>(); // 최단거리를 구하기 위한 Priority Queue
+		int[] dist = new int[V+1]; // 거리 갱신 배열 
+		boolean[] v = new boolean[V+1]; // 방문 체크 
+		Arrays.fill(dist, Integer.MAX_VALUE); // 모든 거리를 최댓값으로 초기화 
 		
 		pq.add(new Node(start, 0)); // 시작점 거리를 0으로 값 넣기 
 		
-		dist[start] = 0;
+		dist[start] = 0; // 시작 위치의 거리는 0 (자기 자신으로 가는 가중치)
 		
-		while(!pq.isEmpty()) {
-			Node cur = pq.poll();
+		while(!pq.isEmpty()) { // Queue가 빌 때까지 
+			Node cur = pq.poll(); // Queue에 들어간 거리 중 가장 최단거리 노드 추출 
 			
-			v[cur.v] = true;
+			v[cur.v] = true; // 방문 처리 
 			
-			for (Node n : graph.get(cur.v)) {
-				if(!v[n.v]) {
-					int d = dist[cur.v] + n.w;
-					if(dist[n.v] > d) {
-						dist[n.v] = d;
-						pq.add(new Node(n.v, d));
+			for (Node n : graph.get(cur.v)) { // 연결된 노드들 확인 
+				if(!v[n.v]) { // 방문 하지 않은 노드라면 
+					int d = dist[cur.v] + n.w; //현재 노드의 최단거리 + 연결 노드까지의 가중치 
+					if(dist[n.v] > d) { // 현재 연결 노드의 거리보다현재 노드의 최단거리 + 연결 노드까지의 가중치 가 더 짧다면 
+						dist[n.v] = d; // dist 거리 갱신 
+						pq.add(new Node(n.v, d)); // queue에 넣어주기 
 					}
 				}
 			}
